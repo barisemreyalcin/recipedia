4261dfd25868160bff62cbc71f1e2f0e9d868679
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-04-21 18:57:48 +0300

    refactor: import config & helpers and update fetching data

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
@@ -6,13 +8,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-    console.log(res, data);
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -28,6 +24,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`💣💣💣💣 ${err} 💣💣💣💣`);
   }
 };
