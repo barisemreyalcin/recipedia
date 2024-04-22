11b7791a2766551213fa51fa56d8fe431a89c7ff
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-04-22 16:39:11 +0300

    feat(search): implement search functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,11 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
+
 import "core-js/stable"; // polyfilling almost everyting
 import "regenerator-runtime/runtime"; // polyfilling async-awaits
+import { async } from "regenerator-runtime";
 
 const controlRecipes = async function () {
   try {
@@ -21,7 +25,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
+
+    // Render search results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,6 +28,26 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        image: recipe.image_url,
+        publisher: recipe.publisher,
+        title: recipe.title,
+      };
+    });
+  } catch (err) {
+    console.error(`${err} 💣💣💣`);
     throw err;
   }
 };
