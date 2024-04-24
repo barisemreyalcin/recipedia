839be500741f6746d72b47aee0de5de66c435bc8
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-04-24 20:12:27 +0300

    chore(config): add new constants

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
+export const MODAL_CLOSE_SEC = 2;
 export const RESULTS_PER_PAGE = 10;
+export const KEY = "78caea39-4c33-4c44-8076-a490e3615b3f";
