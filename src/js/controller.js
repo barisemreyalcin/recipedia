23d0f808e1f5ae03b2b7740f94eb72e3d4d85b0b
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-04-24 22:15:34 +0300

    feat(upload): update bookmarks and change url after uploading a new recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -103,6 +103,12 @@ const controlAddRecipe = async function (newRecipe) {
     // Success message
     addRecipeView.renderSuccess();
 
+    // Render bookmarks view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, "", `#${model.state.recipe.id}`);
+
     // Close the new recipe form popup
     setTimeout(() => addRecipeView.toggleWindow(), MODAL_CLOSE_SEC * 1000);
   } catch (err) {
